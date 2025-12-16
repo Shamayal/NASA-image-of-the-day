f3aa4e4210d86741cbd465fc83ca4108c53554b5
Shamayal Syed <shamayalsyed9@gmail.com>
2025-12-15 20:12:43 -0500

    Added variables for elements

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const API_Key = "";
 const fetchBtn = document.getElementById("fetchBtn");
 const datePicker = document.getElementById("datePicker");
 
+const imgElement = document.getElementById("iotdImage");
+const titleElement = document.getElementById("title");
+const descElement = document.getElementById("description");
+const hdLink = document.getElementById("hdLink");
+const carousel = document.getElementById("carousel");
+
+let recentImages = [];
