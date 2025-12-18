4987c2b94b43f2f5706599522db1a18cf8ebfc56
Shamayal Syed <shamayalsyed9@gmail.com>
2025-12-17 21:01:45 -0500

    planning out functions

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ const hdLink = document.getElementById("hdLink");
 const carousel = document.getElementById("carousel");
 
 let recentImages = [];
+
+// function to display image
+
+// function to add to carousel
+
+// event listener for button
