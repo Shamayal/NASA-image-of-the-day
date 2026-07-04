3831caa9a13876c3b8fe1f6771db849ebc8e3984
Shamayal Syed <shamayalsyed9@gmail.com>
2026-07-03 20:24:33 -0400

    refactor: restructure frontend into modular client folder and prepare backend separation

---
+++ client/script.js
@@ -0,0 +1,25 @@
+const API_KEY = "";
+const fetchBtn = document.getElementById("fetchBtn");
+const datePicker = document.getElementById("datePicker");
+
+const imgElement = document.getElementById("iotdImage");
+const titleElement = document.getElementById("title");
+const descElement = document.getElementById("description");
+const hdLink = document.getElementById("hdLink");
+const carousel = document.getElementById("carousel");
+
+let recentImages = [];
+
+async function fetchIOTD(date = "") {
+  const url = date ? `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`
+: `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+
+const response = await fetch(url);
+const data = await response.json();
+// display image and carousel
+}
+// function to display image
+
+// function to add to carousel
+
+// event listener for button
